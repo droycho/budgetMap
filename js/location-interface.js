3b44d48b2c8413249bbe16779db871273aed843e
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-06-22 15:35:21 -0700

    add another map. this will find user and draw circle.

--- js/location-interface.js
+++ js/location-interface.js
@@ -1,83 +1,159 @@
-var map;
-var infowindow;
+// Right now it is initializing the dispensary map on page load. once button is clicked, it loads geolocation map centered around user's location, but no other markers.
+// var map;
+// var infoWindow;
 // var service;
+//
+//
+// function initMap() {
+//     var portland = {
+//         lat: 45.5231,
+//         lng: -122.6765
+//     };
+//     console.log("I AM A LOG");
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: portland,
+//         zoom: 15
+//     });
+//
+//     infowindow = new google.maps.InfoWindow();
+//     var service = new google.maps.places.PlacesService(map);
+//     service.nearbySearch({
+//         location: portland,
+//         radius: 1000,
+//         keyword: ['dispensary']
+//     }, callback);
+// }
+//
+// function callback(results, status) {
+//     if (status === google.maps.places.PlacesServiceStatus.OK) {
+//         for (var i = 0; i < results.length; i++) {
+//             createMarker(results[i]);
+//         }
+//     }
+// }
+//
+// function createMarker(place) {
+//     var placeLoc = place.geometry.location;
+//     var marker = new google.maps.Marker({
+//         map: map,
+//         position: place.geometry.location
+//     });
+//
+//     google.maps.event.addListener(marker, 'click', function() {
+//         infowindow.setContent(place.name);
+//         infowindow.open(map, this);
+//     });
+// }
 
-$( document ).ready(function() {
-  $('#locateUser').click(locateUser);
-});
 
-function initMap() {
-    var portland = {
-        lat: 45.5231,
-        lng: -122.6765
-    };
-    console.log("I AM A LOG");
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: portland,
-        zoom: 15
-    });
+function writeAddressName(latLng) {
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({
+          "location": latLng
+        },
+        function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK)
+            document.getElementById("address").innerHTML = results[0].formatted_address;
+          else
+            document.getElementById("error").innerHTML += "Unable to retrieve your address" + "<br />";
+        });
+      }
 
-    infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-        location: portland,
-        radius: 1000,
-        keyword: ['dispensary']
-    }, callback);
-}
+      function geolocationSuccess(position) {
+        var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        // Write the formatted address
+        writeAddressName(userLatLng);
 
-function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
+        var myOptions = {
+          zoom : 16,
+          center : userLatLng,
+          mapTypeId : google.maps.MapTypeId.ROADMAP
+        };
+        // Draw the map
+        var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+        // Place the marker
+        new google.maps.Marker({
+          map: mapObject,
+          position: userLatLng
+        });
+        // Draw a circle around the user position to have an idea of the current localization accuracy
+        var circle = new google.maps.Circle({
+          center: userLatLng,
+          radius: position.coords.accuracy,
+          map: mapObject,
+          fillColor: '#0000FF',
+          fillOpacity: 0.5,
+          strokeColor: '#0000FF',
+          strokeOpacity: 1.0
+        });
+        mapObject.fitBounds(circle.getBounds());
+      }
+
+      function geolocationError(positionError) {
+        document.getElementById("error").innerHTML += "Error: " + positionError.message + "<br />";
+      }
+
+      function geolocateUser() {
+        // If the browser supports the Geolocation API
+        if (navigator.geolocation)
+        {
+          var positionOptions = {
+            enableHighAccuracy: true,
+            timeout: 10 * 1000 // 10 seconds
+          };
+          navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
         }
-    }
-}
+        else
+          document.getElementById("error").innerHTML += "Your browser doesn't support the Geolocation API";
+      }
 
-function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
+      window.onload = geolocateUser;
 
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-    });
-}
+
+
+
+
+
+
+
+
 //This part is for Geolocation, to find user's location. locatUser is called upon button click.
-function locateUser() {
-  if (navigator.geolocation){
-    var positionOptions = {
-      enableHighAccuracy: true,
-      timeout: 10 * 1000
-    };
-    navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
-  }
-  else {
-    alert("Your browser doesn't support the Geolocation API");
-  }
-}
-
-function geolocationSuccess(position) {
-
-  var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-  var myOptions = {
-    zoom : 16,
-    center : userLatLng,
-    mapTypeId : google.maps.MapTypeId.ROADMAP
-  };
-
-  var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
-
-  new google.maps.Marker({
-    map: mapObject,
-    position: userLatLng
-  });
-}
-
-function geolocationError(positionError) {
-  alert(positionError);
-}
+//
+// $(document).ready(function() {
+//   $('#locateUser').click(locateUser);
+// });
+//
+// function locateUser() {
+//   if (navigator.geolocation){
+//     var positionOptions = {
+//       enableHighAccuracy: true,
+//       timeout: 10 * 1000
+//     };
+//     navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
+//   }
+//   else {
+//     alert("Your browser doesn't support the Geolocation API");
+//   }
+// }
+//
+// function geolocationSuccess(position) {
+//
+//   var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+//
+//   var myOptions = {
+//     zoom : 16,
+//     center : userLatLng,
+//     mapTypeId : google.maps.MapTypeId.ROADMAP
+//   };
+//
+//   var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+//
+//   new google.maps.Marker({
+//     map: mapObject,
+//     position: userLatLng
+//   });
+// }
+//
+// function geolocationError(positionError) {
+//   alert(positionError);
+// }
