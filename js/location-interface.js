db7b60dae7c656c466df4c5b16cca102cecc095b
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-06-22 08:18:00 -0700

    current code will display nearest stores to hard-coded location.

--- js/location-interface.js
+++ js/location-interface.js
@@ -1,72 +1,79 @@
 var map;
 var infowindow;
-
-$( document ).ready(function() {
-  $('#locateUser').click(locateUser);
-});
+// var service;
 
 function initMap() {
-  var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: userLatLng,
-    zoom: 15
-  });
-
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch({
-    location: userLatLng,
-    radius: 500,
-    type: ['park']
-  }, callback);
-}
-
-//google maps functions
-function locateUser() {
-  // If the browser supports the Geolocation API
-  if (navigator.geolocation){
-    var positionOptions = {
-      enableHighAccuracy: true,
-      timeout: 10 * 1000 // 10 seconds
+    var pyrmont = {
+        lat: -33.867,
+        lng: 151.195
     };
-    navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
-  }
-  else {
-    alert("Your browser doesn't support the Geolocation API");
-  }
+    console.log("I AM A LOG");
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 15
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: pyrmont,
+        radius: 1000,
+        types: ['store']
+    }, callback);
 }
 
-
-
-// this is the success callback from telling the navigator (your browser) to get the current user's position
-// we do this on line 13 above. We pass in a function to call on success, a function to call on error, and some options to tell the geolocation api how we want it to run.
-// on successfully locating the user, geolocationSuccess() gets called automatically, and it is passed the user's position as an argument.
-// on error, geolocationError is called.
-
-
-function geolocationSuccess(position) {
-  // here we take the `position` object returned by the geolocation api
-  // and turn it into google maps LatLng object by calling the google.maps.LatLng constructor function
-  // it 2 arguments: one for latitude, one for longitude.
-  // You could refactor this section to pass google maps your own coordinates rather than using geolocation for the user's current location.
-  // But you must use coordinates to use this method.
-  var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-  var myOptions = {
-    zoom : 16,
-    center : userLatLng,
-    mapTypeId : google.maps.MapTypeId.ROADMAP
-  };
-  // Draw the map - you have to use 'getElementById' here.
-  var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
-  // Place the marker
-  new google.maps.Marker({
-    map: mapObject,
-    position: userLatLng
-  });
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
 }
 
-function geolocationError(positionError) {
-  alert(positionError);
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
 }
+//This part is for Geolocation, to find user's location. locatUser is called upon button click.
+// function locateUser() {
+//   if (navigator.geolocation){
+//     var positionOptions = {
+//       enableHighAccuracy: true,
+//       timeout: 10 * 1000
+//     };
+//     navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
+//   }
+//   else {
+//     alert("Your browser doesn't support the Geolocation API");
+//   }
+// }
+//
+// function geolocationSuccess(position) {
+//
+//   var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+//
+//   var myOptions = {
+//     zoom : 16,
+//     center : userLatLng,
+//     mapTypeId : google.maps.MapTypeId.ROADMAP
+//   };
+//
+//   var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+//
+//   new google.maps.Marker({
+//     map: mapObject,
+//     position: userLatLng
+//   });
+// }
+//
+// function geolocationError(positionError) {
+//   alert(positionError);
+// }
